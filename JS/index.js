8a350e27e579cb831903c6278e3bc56e3c27e513
zanm3 <zan.markuza@tscng.org>
2025-05-12 18:53:30 +0100

    update

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,11 @@ playerFormDva.style.visibility = "hidden";
 const playerFormTri = document.getElementById("playerFormTri");
 playerFormTri.style.visibility = "hidden";
 
-document.getElementById("add").addEventListener("click", function(){
+document.getElementById("addEna").addEventListener("click", function(){
     playerFormDva.style.visibility = "visible";
+    
+});
+document.getElementById("addDva").addEventListener("click", function(){
+    playerFormTri.style.visibility = "visible";
     
 });
