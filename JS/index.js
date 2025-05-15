17bc8cbc70b1214138b0ddde7c1d072f8dcace3f
zanm3 <zan.markuza@tscng.org>
2025-05-15 17:37:27 +0200

    update

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,33 @@
-const playerFormDva = document.getElementById("playerFormDva");
-playerFormDva.style.visibility = "hidden";
+const firstForm = document.getElementById("playerFormEna");
+const secondForm = document.getElementById("playerFormDva");
+const thirdForm = document.getElementById("playerFormTri");
 
-const playerFormTri = document.getElementById("playerFormTri");
-playerFormTri.style.visibility = "hidden";
+const players = [
+    document.getElementById("playerFormEna"),
+    document.getElementById("playerFormDva"),
+    document.getElementById("playerFormTri")
+];
 
-document.getElementById("addEna").addEventListener("click", function(){
-    playerFormDva.style.visibility = "visible";
-    
+firstForm.style.display = "none";
+secondForm.style.display = "none";
+thirdForm.style.display = "none";
+
+let playersAdd = 0;
+document.getElementById("add").addEventListener("click", function(){
+    if(playersAdd < players.length){
+        players[playersAdd].style.display = "block";
+        playersAdd += 1;
+    }
+    if(playersAdd == players.length)
+        playersSub = players.length - 1;
 });
-document.getElementById("addDva").addEventListener("click", function(){
-    playerFormTri.style.visibility = "visible";
-    
+
+let playersSub = 2;
+document.getElementById("sub").addEventListener("click", function(){
+    if(playersSub >= 0){
+        players[playersSub].style.display = "none";
+        playersSub--;
+    }
+    if(playersSub == 0)
+        playersAdd = 0;
 });
